a5fcc2456d5e6c35f559208467474a69e25eab53
sagar rathna sabapathy <sagar.rathnasabapathy@diconium.com>
2019-03-18 21:10:01 +0100

    setup ideaEntity

--- src/idea/idea.service.ts
+++ src/idea/idea.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IdeaEntity } from './idea.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class IdeaService {}
+export class IdeaService {
+    constructor(
+        @InjectRepository(IdeaEntity)
+        private ideaRepository: Repository<IdeaEntity>,
+    ){}
+}
