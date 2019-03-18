611ce29ecf6d86a73a097ac49b29eee0c4ef0252
sagar rathna sabapathy <sagar.rathnasabapathy@diconium.com>
2019-03-18 21:21:01 +0100

    service methods for idea module

--- src/idea/idea.service.ts
+++ src/idea/idea.service.ts
@@ -9,4 +9,30 @@ export class IdeaService {
         @InjectRepository(IdeaEntity)
         private ideaRepository: Repository<IdeaEntity>,
     ){}
+
+    async showAll () {
+        return await this.ideaRepository.find();
+    }
+
+    async create (data) {
+        const idea = await this.ideaRepository.create(data);
+        await this.ideaRepository.save(idea);
+        return idea;
+    }
+
+    async read (id: string) {
+        return await this.ideaRepository.findOne({ where: { id }});
+    }
+
+    async update (id: string, data) {
+        await this.ideaRepository.update({ id }, data);
+        return await this.ideaRepository.findOne({ id });
+    }
+
+    async destroy (id: string) {
+        await this.ideaRepository.delete({ id });
+        return {
+            deleted: true
+        };
+    }
 }
