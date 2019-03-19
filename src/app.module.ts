96df52d8e0102304e147ef03669be2954b670140
sagar rathna sabapathy <sagar.rathnasabapathy@diconium.com>
2019-03-19 12:49:03 +0100

    added Logging interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IdeaModule } from './idea/idea.module';
 import { HttpErrorFilter } from './shared/http-error.filter';
+import { LoggingInterceptor } from './shared/logging.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), IdeaModule],
@@ -13,6 +14,9 @@ import { HttpErrorFilter } from './shared/http-error.filter';
   providers: [AppService, {
     provide: APP_FILTER,
     useClass: HttpErrorFilter
+  },{
+    provide: APP_INTERCEPTOR,
+    useClass: LoggingInterceptor
   }],
 })
 export class AppModule {}
