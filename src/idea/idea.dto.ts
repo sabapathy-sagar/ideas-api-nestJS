add26b069f636b878663e0be73274381ea9cb7ce
sagar rathna sabapathy <sagar.rathnasabapathy@diconium.com>
2019-03-22 20:57:19 +0100

    user & idea response objects

--- src/idea/idea.dto.ts
+++ src/idea/idea.dto.ts
@@ -1,9 +1,18 @@
 import { IsString } from 'class-validator';
-
+import { UserRO } from '../user/user.dto';
 export class IdeaDTO {
     @IsString()
     idea: string;
 
     @IsString()
     description: string;
+}
+
+export class IdeaRO {
+    id?: string;
+    updated:string;
+    created: string;
+    idea: string;
+    description: string;
+    author: UserRO
 }
--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -6,3 +6,10 @@ export class UserDTO {
     @IsNotEmpty()
     password: string;
 }
+
+export class UserRO {
+    id: string;
+    username: string;
+    created: Date;
+    token?: string;
+}
